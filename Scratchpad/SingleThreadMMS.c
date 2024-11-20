7e8ffcd26400d2e86dba42f6c4cd1a74b1b9ed91
Dana Maloney <danamon2002@gmail.com>
2024-11-19 23:56:07 -0500

    Single thread MMS first fit functional.

--- Scratchpad/SingleThreadMMS.c
+++ Scratchpad/SingleThreadMMS.c
@@ -3,15 +3,16 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
+#include <stdint.h>
 
 #define MAX_SIZE (30 * 1024) // 30MB
-#define MAX_BLOCKS 10 // Maximum number of blocks in memory.
+#define MAX_BLOCKS 5 // Maximum number of blocks in memory.
 
 struct DataBlock {
 	int id;
 	int in_use;
-	void *blockStart;
-	void *blockEnd;
+	void *block_start;
+	void *block_end;
 } DataBlock;
 
 //Single threaded memory manager test.
@@ -22,9 +23,25 @@ void *end = NULL;
 
 struct DataBlock memoryTable[MAX_BLOCKS]; // array of data blocks.
 
-int block_size(struct DataBlock *block){
-/* Calculates size of a DataBlock. */
-	return (char *)block->blockEnd - (char *)block->blockStart;
+
+void print_data_block(struct DataBlock *block) {
+    if (block == NULL) {
+        printf("Invalid DataBlock (NULL pointer).\n");
+        return;
+    }
+
+    printf("DataBlock Information:\n");
+    printf("ID: %d\n", block->id);
+    printf("In Use: %s\n", block->in_use ? "Yes" : "No");
+    printf("Block Start Address: %p\n", block->block_start);
+    printf("Block End Address: %p\n", block->block_end);
+
+    // Calculate and print the size of the block
+    if (block->block_start != NULL && block->block_end != NULL) {
+        printf("Block Size: %ld bytes\n", (char *)block->block_end - (char *)block->block_start);
+    } else {
+        printf("Block Size: Unknown (start or end is NULL)\n");
+    }
 }
 
 void initialize_memory(){
@@ -33,6 +50,72 @@ void initialize_memory(){
 	end = (void *)((char *)start + MAX_SIZE); // use char pointer because it moves 1 byte at a time.
 	printf("Memory starts at %p\n", start);
 	printf("Memory ends at %p\n", end);
+	printf("Memory available: %lu bytes\n", (uintptr_t)end - (uintptr_t)start);
+}
+
+void *block_start(int index){
+	/* finds start of current block. */
+	
+	printf("\n!!!!!!!!!!!Finding start for block at index %d !!!!!!!!!!!!!\n", index);
+	
+	void *result = NULL;
+	
+	for(int i = index; i >= 0; i--){
+		if(i - 1 < 0){
+			result = start;
+			printf("Result is start.\n");
+			break;
+		} else { // if you're not in the first block, look at the end of the block before you.
+			if(memoryTable[i - 1].in_use != 0){
+				printf("Block %d is in use.\n", i - 1);
+				result = memoryTable[i - 1].block_end + 1;
+				printf("i = %d, result = %lu\n", i, result);
+				break;
+			} else {
+				printf("Block %d isn't in use.\n", i - 1);
+			}
+		}
+	}
+	
+	printf("Block %d's start address is %lu\n", index, (uintptr_t)result);
+	printf("Start is equal to %lu\n\n", (uintptr_t)start);
+	return result;
+}
+
+void *block_max_end(int index){
+	/* finds max possible end of current block. */
+	
+	//printf("\nFinding max end for block at index %d\n", index);
+	
+	void *result = NULL;
+	
+	for(int i = index + 1; i < MAX_BLOCKS; i++){// work forwards to find end.
+		if(i >= MAX_BLOCKS - 1){// if in the last block, max end is end of memory.
+			result = end;
+			//printf("Result is end.\n");
+		}
+		else if(memoryTable[i].in_use != 0){
+			result = memoryTable[i].block_start - 1;
+			//printf("Result from block %d and is %lu\n", i, (uintptr_t)result);
+		}
+	}	
+	//printf("Block %d's max end address is %lu\n", index, (uintptr_t)result);
+	//printf("End is equal to %lu\n\n", (uintptr_t)end);
+	return result;
+}
+
+uintptr_t available_space(int index){
+/* return, in bytes, the amount of space available at an index. */
+
+	void *space_start = start;
+	void *space_end = end;
+	
+	space_end = block_max_end(index);
+	space_start = block_start(index);
+
+	uintptr_t space = (uintptr_t)space_end - (uintptr_t)space_start;
+	printf("%lu - %lu = %lu\n", (uintptr_t)space_end, (uintptr_t)space_start, space);
+	return space;
 }
 
 void *first_fit(int size, int id){
@@ -43,15 +126,26 @@ void *first_fit(int size, int id){
 
 		for(int i = 0; i < MAX_BLOCKS; i++){ //look for first available block.
 			printf("Current Block: %d\n", i);
+			
 			if (memoryTable[i].in_use == 0){ // check for open block
 				printf("OPEN BLOCK AT INDEX %d\n", i);
-				//TODO run checks for if there is enough space for block.
-				return NULL;
+				
+				// Run checks for if there is enough space for block.
+				printf("Check space for block at index %d\n", i);
+				if (size <= available_space(i)){  // enough space
+					//put block at this index!
+					memoryTable[i].id = id;
+					memoryTable[i].in_use = 1;
+					memoryTable[i].block_start = block_start(i);
+					memoryTable[i].block_end = block_start(i) + size;
+				}
+				return NULL; // TODO return success
 			} else {
 				printf("Block %d is taken.\n", i);
 			}
 		}
 		//if you get here, it means you went through all the blocks and there was no room.
+		printf("No room.\n");
 		return NULL;
 	} else {
 		printf("Number is not power of 2\n");
@@ -69,8 +163,13 @@ int main(){
 		break;
 	}
 	initialize_memory();
-	first_fit(4, 21);
+	first_fit(4, 1);
+	first_fit(1024, 2);
+	first_fit(64, 2);
 	first_fit(19, 1);
+	for(int i = 0; i < MAX_BLOCKS; i++){
+		print_data_block(&memoryTable[i]);
+	}
 	free_memory();
 	return 0;
 }
