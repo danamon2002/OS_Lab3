df155beadcd5d9bd20efce20a8184b5232193f40
Dana Maloney <danamon2002@gmail.com>
2024-11-30 00:04:27 -0500

    WIP Buffer system. producers (users) can make requests with size and ID

--- Lab_3.c
+++ Lab_3.c
@@ -9,9 +9,28 @@
 #include<signal.h>
 #include "MMS.h"
 
+// Define buffer
+typedef struct {
+    int size;
+    int id;
+} BufferItem;
+
+#define BUFFER_SIZE 10
+BufferItem buffer[BUFFER_SIZE]; // buffer for memory requests.
+int in = 0; // Index for the producer to insert items
+
+// Define sync tools (sem, mux)
+sem_t empty;       // Tracks available slots in the buffer
+sem_t full;        // Tracks filled slots in the buffer
+pthread_mutex_t mutx; // Ensures mutual exclusion
+
 /* Function defs below  vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv */
 
 void *user_thread(void *arg){
+    /* User thread:
+     * Should place requests for memory into buffer.
+     * Then, go to sleep.
+     * Then, wake up and free memory.*/
 	int num = *(int *)arg; // Cast and dereference the argument
 	int id = pthread_self();
     srand(id);
@@ -21,17 +40,34 @@ void *user_thread(void *arg){
     for(int i = 0; i < power; i++)
         size *= 2;
 
-    sleep(1); // let the MMS thread start. TODO make thread put request in buffer.
-    first_fit(size, id);
+    sleep(1); // let the MMS thread start.
+    // Make request item for buffer.
+    BufferItem request_block;
+    request_block.size = size;
+    request_block.id = num; // TODO change to thread ID?
+    //secure buffer for puttinng item in.
+    sem_wait(&empty);
+    while( pthread_mutex_lock(&mutx)){ // TODO verify that this works: Should loop until mutex lock obtained
+        //do nothing.
+    }
+    // make thread put request in buffer.
+    buffer[in] = request_block;
+    in = (in + 1) % BUFFER_SIZE;
+    //release mutex.
+    if (pthread_mutex_unlock(&mutx)){
+        perror("Failed to release mutex from a consumer");
+    }
+    // go eepy.
 	printf("I am thread #%d going to sleep.\n", id);
     sleep(sleepTime);
+    //free up memory.
     free_block(id);
 	free(arg); // Free the dynamically allocated memory
     printf("I am thread #%d Waking Up.\n");
 	return NULL;
 }
 
-void create_threads(int n) {
+void run_threads(int n) {
 	/* Function to create n+1 threads. */
 
    	pthread_t *threads = malloc((n + 1) * sizeof(pthread_t)); // Array to store thread IDs
@@ -87,8 +123,24 @@ int main(int argc, char *argv[]){
 	} else {
 		int arg_1_int = atoi(argv[1]);
 		printf("Argument was %d\n", arg_1_int);
+        //Init mutex
+        if(pthread_mutex_init(&mutx, NULL)) {
+            puts("Error mutex initialization");
+        }
+
+        // Lock/unlock functions:
+        // pthread_mutex_lock(&mutx);	// mutex lock before critical section
+        // pthread_mutex_unlock(&mutx);	// mutex unlock
+
+
 		// call function to make n+1 threads.
-		create_threads(arg_1_int);
+		run_threads(arg_1_int);
+
+        //breakdown mutex.
+        pthread_mutex_destroy(&mutx);	// destroy mutex
+
+        //print final state: If all threads are done, there should be NO memory blocks in use!
+        printf("FINAL STATE:\n");
 		for(int i = 0; i < MAX_BLOCKS; i++){
             print_data_block(&memoryTable[i]);
         }
